76a048db50fc3d88757c1668a1108407ec4e408d
Александр Шмелев <120136549+Felarn@users.noreply.github.com>
2023-10-24 21:09:03 +0300

    done

--- src/ObjectSchema.js
+++ src/ObjectSchema.js
@@ -3,19 +3,13 @@ export default class ObjectSchema {
     this.template = [];
     this.validators = [
       (objToCheck) => typeof objToCheck === "object" && objToCheck !== null,
-      (objToCheck) => {
-        // console.log(objToCheck);
-        // console.log(Object.keys(this.template).length);
-        // console.log(Object.keys(objToCheck).length);
-        Object.keys(this.template).length === Object.keys(objToCheck).length;
-      },
+      (objToCheck) =>
+        Object.keys(this.template).length === Object.keys(objToCheck).length,
     ];
   }
 
   shape(template) {
     this.template = template;
-    // console.log("===============================>", template);
-
     this.validators.push((objToCheck) =>
       Object.entries(objToCheck).every(([key, value]) => {
         console.log(key, value);
